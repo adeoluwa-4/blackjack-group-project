ce9a32ba096dc152e9d6bfcb297840bca12b015a
christianrawalt <christianrawaltschool@gmail.com>
2025-11-10 15:10:22 -0600

    message

--- src/BlackjackGame/Game/Input.cs
+++ src/BlackjackGame/Game/Input.cs
@@ -9,13 +9,13 @@ namespace BlackjackGame.Game
             // TODO: loop until non-empty
             throw new NotImplementedException();
         }
-
+        //change
         public static int ReadIntInRange(string prompt, int min, int max)
         {
             // TODO: loop until valid int in [min,max]
             throw new NotImplementedException();
         }
-
+            //hi
         public static bool ReadYesNo(string prompt)
         {
             // TODO: loop until 'y' or 'n'
